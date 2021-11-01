f9b73be31a3548cb92bbe1159db39b51bd2ce7c1
javierpb275 <javierpb275@gmail.com>
2021-11-01 19:24:27 +0100

    dynamic components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>hello</div>
+  <div>
+    <select v-model="componentName">
+      <option value="Home">Home</option>
+      <option value="About">About</option>
+    </select>
+    <keep-alive>
+      <component :is="componentName"></component>
+    </keep-alive>
+  </div>
 </template>
 
 <script>
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+
 export default {
   name: "App",
+  components: {
+    Home,
+    About,
+  },
+  data() {
+    return {
+      componentName: "Home",
+    };
+  },
 };
 </script>
